b57500c1039e5b079cfd12caa88ba065535c9aca
JeremyFerreira <91945288+JeremyFerreira@users.noreply.github.com>
2022-10-14 15:25:51 +0200

    PlayerMovementProto

---
+++ Assets/Script/ShipController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.InputSystem;
+
+public class ShipController : MonoBehaviour
+{
+    private Camera _camera;
+    [SerializeField] InputAsset input;
+    [SerializeField] LayerMask layerMask;
+
+    [SerializeField] float speed = 5;
+    Vector3 lastHitPoint;
+
+    private void Awake()
+    {
+        input = new InputAsset();
+        _camera = Camera.main;
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FollowCursor();
+
+    }
+    
+    public void FollowCursor()
+    {
+        Vector3 direction = new Vector3((MouseToWorldPosition().x - transform.position.x), 0, (MouseToWorldPosition().z - transform.position.z));
+        Debug.Log(direction.magnitude);
+        if(direction.magnitude >0.2f)
+        {
+            transform.position += direction.normalized * speed * Time.deltaTime;
+        }
+        
+    }
+        /// <summary> Retourne la position de la souris dans le monde 3D en fonction du hit du raycast</summary> 
+    Vector3 MouseToWorldPosition()
+    {
+        RaycastHit RayHit;
+        Ray ray;
+        Vector3 Hitpoint = Vector3.zero;
+        // On trace un rayon avec la mousePosition de la souris
+        ray = _camera.ScreenPointToRay(input.Game.Cursor.ReadValue<Vector2>());
+        if (Physics.Raycast(ray, out RayHit, Mathf.Infinity, layerMask))
+        {
+            Hitpoint = new Vector3(RayHit.point.x, RayHit.point.y, RayHit.point.z);
+
+            if (Hitpoint != null)
+                Debug.DrawLine(_camera.transform.position, Hitpoint, Color.blue, 0.5f);
+            lastHitPoint = Hitpoint;
+            return Hitpoint;
+        }
+        else
+        {
+            
+            return lastHitPoint;
+        }
+
+        
+    }
+}
+
