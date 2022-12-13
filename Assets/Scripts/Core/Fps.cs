3ad39500e4095a5dfd094ae0bdb4acb9aca59f2e
clixmods <clix.mods.cod@gmail.com>
2022-12-13 17:58:47 +0100

    Update map with music

---
+++ Assets/Scripts/Core/Fps.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class Fps : MonoBehaviour
+{
+    private float count;
+    
+    private IEnumerator Start()
+    {
+        GUI.depth = 2;
+        while (true)
+        {
+            count = 1f / Time.unscaledDeltaTime;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    
+    private void OnGUI()
+    {
+        Rect location = new Rect(5, 5, 85, 25);
+        string text = $"FPS: {Mathf.Round(count)}";
+        Texture black = Texture2D.linearGrayTexture;
+        GUI.DrawTexture(location, black, ScaleMode.StretchToFill);
+        GUI.color = Color.black;
+        GUI.skin.label.fontSize = 18;
+        GUI.Label(location, text);
+    }
+
+    private void OnDestroy()
+    {
+        StopCoroutine(Start());
+    }
+}
